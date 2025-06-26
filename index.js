ec23a17f9926099fc999573eaace5697c200a33c
storres20 <sanchotorres777@gmail.com>
2025-06-26 13:31:39 -0500

    Update PORT

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const WebSocket = require('ws');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const PORT = 3005;
+const PORT = process.env.PORT || 3005;
 const DEVICES_FILE = path.join(__dirname, 'data', 'devices.json');
 
 app.use(express.urlencoded({ extended: true }));
